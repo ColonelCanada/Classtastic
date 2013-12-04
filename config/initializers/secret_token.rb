c4e32283a99f02438394a6cb9c678cc8993033de
Kody Pomelow <kdoypomelow11@gmail.com>
2013-12-04 14:56:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dayman::Application.config.secret_key_base = '7444b54b2a124d00997c2b8278e47b1bf503f75338cb7c3cfaeccd1afe8cde7f8c31e4ea7bf0c7f2102a0007d183ddd0a027c4a929db065bf32815b38f44402a'
